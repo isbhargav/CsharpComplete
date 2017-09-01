815ebdf739e7f780eeeca96559aeff62b6c5b085
Bhargav Lad <ladbhargav@yahoo.co.in>
2017-09-01 18:22:51 +0530

    Customer class added

--- Program.cs
+++ Program.cs
@@ -2,12 +2,30 @@
 
 namespace CsharpComplete
 {
-    
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-      
-        }
-    }
+	public class Customer
+	{
+		string firstname, LastName;
+		double total;
+		//Constructor
+		public void abc_Customer(string f, string l, double t)
+		{
+			this.firstname = f;
+			this.LastName = l;
+			this.total = t;
+		}
+		public string printDetails()
+		{
+			return firstname + " " + LastName + " " + total.ToString();
+		}
+
+	}
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//     Customer c = new Customer("Bharagv","Lad",200.21);
+			Customer a = new Customer();
+			Console.WriteLine(a.printDetails());
+		}
+	}
 }
